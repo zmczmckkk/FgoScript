1f523f695e01bdb863a185df8ee91ea31637b08b
RENZHEHAO <zmczmckkk@outlook.com>
2019-03-27 21:00:02 +0800

    Amend the task execution plan so that it does not perform tasks that have expired.

---
+++ src/fgoScript/entity/guda/ExpApGudaziForMission.java
@@ -0,0 +1,12 @@
+package fgoScript.entity.guda;
+
+import fgoScript.constant.PointInfo;
+
+import java.awt.*;
+
+public class ExpApGudaziForMission extends QpApGudazi {
+    @Override
+    public void intoAndSelect(int apNum) throws Exception {
+        new ExpApGudazi().intoAndSelect(apNum);
+    }
+}
